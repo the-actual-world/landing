21d914ac79c36d22ae5ead6e50e50fca01fac88b
Kraktoos <kraktoos@gmail.com>
2024-07-04 15:22:00 +0100

    Hide errors

--- index.php
+++ index.php
@@ -3,10 +3,10 @@ include_once 'include/config.inc.php';
 
 $path = $_GET['path'] ?? 'index';
 
-// error_reporting(0);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+error_reporting(0);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 if (strpos($path, 'admin') != false) {
   exit;
